334f47f0ba3b195398fc02bcd1277c3345c8b83c
unknown <ncfcdaniel@outlook.com>
2019-11-08 14:20:12 +0000

    Completed project

--- Program.cs
+++ Program.cs
@@ -6,24 +6,23 @@ namespace WithLiskov
     {
         static void Main(string[] args)
         {
-            var numbers = new int[] { 1,2,3,4,5 };
+            var numbers = new int[] { 1,2,3,4,5 }; //The chosen numbers
 
-            Calculator sum = new SumCalculator(numbers);
             
  
 
             Calculator evenSum = new EvenNumbersSumCalculator(numbers);
-            Console.WriteLine($"The sum of all the even numbers:  {evenSum.Calculate()}");
+            Console.WriteLine($"The sum of all the even numbers:  {evenSum.Calculate()}"); //Outputs the sum of the even numbers
             
             Console.WriteLine ();
 
             Calculator oddSum = new OddNumbersSumCalculator(numbers);
-            Console.WriteLine($"The sum of all the odd numbers:  {oddSum.Calculate()}");
+            Console.WriteLine($"The sum of all the odd numbers:  {oddSum.Calculate()}");//Outputs the sum of the odd numbers
 
 
             Console.WriteLine ();
-
-                       Console.WriteLine($"The sum of all the numbers: {sum.Calculate()}");
+            Calculator sum = new SumCalculator(numbers); //creates an object sumcalculator
+            Console.WriteLine($"The sum of all the numbers: {sum.Calculate()}"); //Outputs the sum of numbers
 
         }
     }
